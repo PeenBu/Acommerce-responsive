192855cd1eac372eede49138e8fa8f91dedf14a8
Paveen <peen_b@hotmail.com>
2019-11-13 23:04:06 +0700

    lool

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,67 @@
 <template>
-  <v-app>
+<div>
+  <p style="padding-left:5%; padding-top:2%;">
+    Items 1 - {{Id.length}} of {{Id.length}}
+    </p>
+  <v-app style="padding:2% 5% 2% 5%; backgroundColor:white;">
+    <hr style="border: 1px solid rgb(216,216,216)	;">
     <v-row
       class="mb-6"
       no-gutters
     >
       <v-col
         v-for="info in Id" :key="info.id" class="informaiton"
-        cols = "3"
+        cols="12"
+        xs="12"
+        sm="3"
+        lg="3"
+        md="3"
+        xl="3"
       >
         <!-- This is where to put card in  -->
     <v-card
     :loading="loading"
-    class="mx-1 my-1"
-    max-width="350"
     height="550"
-    >
-    <v-flex>
-    <v-img
-      height="250"
-      :src="info.image_url"
-    ></v-img>
-     <p>ID: {{info.id}}</p>
-    <v-card-title>{{info.title}}</v-card-title>
-    <v-card-title>{{info.created_at}}</v-card-title>
-    <v-card-text>
-      <v-row
-        align="center"
-        class="mx-0"
-      >
-        <v-rating
-          :value=info.vote
-          color="red"
-          dense
-          half-increments
-          readonly
-          size="17"
-        ></v-rating>
+    flat="true" 
+    hover="true"
+    style="margin-right:3px; margin-bottom:3px" >
+    <v-flex style="padding:5%; ;">
+      <div>
+          <v-img
+            height="250"
+            :src="info.image_url"
+          ></v-img>
+          <p style="font-size: 17px;">{{info.title}}</p>
+          <p style="font-size: 14px;">{{
+            today.diff(moment(info.created_at), 'weeks') == 1 ? `${today.diff(moment(info.created_at), 'week')} week`
+            : `${today.diff(moment(info.created_at), 'week')} weeks`
+            }} ago</p>
+          <v-card-text>
+            <v-row
+              align="left"
+              
+            >
+              <v-rating
+                :value=info.vote
+                color="red"
+                dense
+                half-increments
+                readonly
+                size="17"
+              ></v-rating>
 
-      </v-row>
-
-      <div class="my-4 subtitle-1 black--text">	&#3647;
-         {{info.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}}
+            </v-row>
+            
+         </v-card-text>
+         <p style="text-align: left;font-size: 19px">&#3647;
+              {{info.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}}.00
+            </p>
       </div>
-    </v-card-text>
     </v-flex>
   </v-card>
       </v-col>
     </v-row>
-    <List>
-      
-    </List>
+    
      <div class="text-center">
     <v-pagination
       v-model="page"
@@ -58,23 +70,26 @@
       next-icon="mdi-menu-right"
     ></v-pagination>
   </div>
+  <hr style="border: 1px solid rgb(216,216,216)	; margin-top:1%">
   </v-app>
-  
+</div>
 </template>
 
 
 <script>
-import List from './components/list';
 import data from "./assets/list.json";
+var moment = require('moment');
 export default {
   name: 'app',
   components: {
-    List,
+    
   },
 
   data: () => ({
      Id : data,
      page: 1,
+     moment: moment,
+     today : moment(),
   }),
 };
 </script>
